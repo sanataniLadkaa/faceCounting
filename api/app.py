4489490a332c9ef1e6103017941073e38d4841c7
Anurag Tiwari <150263465+sanataniLadkaa@users.noreply.github.com>
2024-11-11 10:48:02 +0530

    Update app.py

--- api/app.py
+++ api/app.py
@@ -59,4 +59,4 @@ def uploaded_file(filename):
     return render_template('result.html', filename=filename)
 
 if __name__ == '__main__':
-    app.run(host='192.168.97.66', port=5000)
+    app.run(host='0.0.0.0', port=5000)
